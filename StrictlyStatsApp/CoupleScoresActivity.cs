e828878324853a37beb8c1b9a963f43488a941c1
Andrew Riggs <andrew.riggs@atkinsglobal.com>
2021-04-28 13:17:10 +0100

    Added logic to add selected couple weekly score and the total and average as text in the activity

--- StrictlyStatsApp/CoupleScoresActivity.cs
+++ StrictlyStatsApp/CoupleScoresActivity.cs
@@ -35,6 +35,45 @@ namespace StrictlyStats
 
             // Update Selected Couple Text
             FindViewById<TextView>(Resource.Id.selectedCoupleTextView).Text = $"Selected Couple: {selectedCouple.ToString()}";
+
+            // Field to store the couple's total score and number of them (for average calc)
+            double totalScore = 0;
+            int scoreCount = 0;
+
+            // Add individual dance scores
+            
+            // Iterate over the all scores
+            foreach (Score score in uow.Scores.GetAll())
+            {
+                // If the score is for this couple
+                if (score.CoupleID == selectedCouple.CoupleID)
+                {
+                    // Add text view with the week number, dance and score data
+                    AddTextView($"Week {score.WeekNumber}:\n" +
+                        $"Dance - {uow.Dances.GetById(score.DanceID).DanceName}\n" +
+                        $"Score - {score.Grade}\n");
+
+                    // Update total score and score count totals
+                    totalScore += score.Grade;
+                    scoreCount++;
+                }
+            }
+
+            // Calculate and add average score text
+            AddTextView($"Total: {totalScore}\n" +
+                $"Average: {(totalScore/scoreCount).ToString()}");
+        }
+
+        private void AddTextView(string text)
+        {
+            // Create new textview object in the context of this activity
+            TextView textView = new TextView(this);
+            // Set the textview text
+            textView.Text = text;
+            // Set the textview style#
+            textView.TextSize = 20;
+            // Add the textview to the existing linear layout in the activity
+            FindViewById<LinearLayout>(Resource.Id.coupleScoresLinearLayout).AddView(textView);
         }
     }
 }
