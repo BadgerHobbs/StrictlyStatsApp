e63f183bd7ee162db70603011409305770c2d304
Andrew Riggs <andrew.riggs@atkinsglobal.com>
2021-05-03 02:20:00 +0100

    Added buttons to couples administration page as well as starting logic for the addition of dropdown menus

--- StrictlyStatsApp/CouplesAdministrationActivity.cs
+++ StrictlyStatsApp/CouplesAdministrationActivity.cs
@@ -10,16 +10,34 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
+using StrictlyStatsDataLayer;
+using StrictlyStatsDataLayer.Models;
+
 namespace StrictlyStats
 {
     [Activity(Label = "CouplesAdministrationActivity")]
     public class CouplesAdministrationActivity : Activity
     {
+        IStrictlyStatsUOW uow = Global.UOW;
+        private List<Couple> couples;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Create your application here
+            SetContentView(Resource.Layout.CouplesAdministration);
+
+            couples = uow.Couples.GetAll();
+
+            foreach (Couple couple in uow.Couples.GetAll())
+            {
+                AddNewListCouple(couple);
+                break;
+            }
+        }
+
+        private void AddNewListCouple(Couple newCouple)
+        {
         }
     }
 }
