e0767d88a011ac1fd46a6cfedfdfcfac286a7510
Andrew Riggs <andrew.riggs@atkinsglobal.com>
2021-06-16 15:32:25 +0100

    Automatic changes

--- StrictlyStatsApp/obj/Debug/90/android/src/android/arch/lifecycle/R.java
+++ StrictlyStatsApp/obj/Debug/90/android/src/android/arch/lifecycle/R.java
@@ -4675,7 +4675,7 @@ containing a value of this type.
         public static int ifRoom=0x7f0a0041;
         public static int image=0x7f0a006e;
         public static int info=0x7f0a00d3;
-        public static int instructionDetailLinearLayout=0x7f0a00ca;
+        public static int instructionDetailScrollView=0x7f0a00ca;
         public static int instructionDetailTextView=0x7f0a00cc;
         public static int instructionHeadingTextView=0x7f0a00cb;
         public static int instructionsButton=0x7f0a009f;
